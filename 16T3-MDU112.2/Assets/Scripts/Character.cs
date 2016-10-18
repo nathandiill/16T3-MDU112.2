9088faf1446f834fd2a1c9186bac13f44f73bbd4
Nathan Diill <Nathan Diill>
2016-10-18 11:03:33 +1000

    Initial commit.

---
+++ 16T3-MDU112.2/Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Character {
+
+    int agility = UnityEngine.Random.Range(1, 11);
+    int stamina = UnityEngine.Random.Range(1, 11);
+    int strength = UnityEngine.Random.Range(1, 11);
+    int acuity = UnityEngine.Random.Range(1, 11);
+
+    public Character (int _agility, int _stamina, int _strength, int _acuity)
+    {
+        agility = _agility;
+        stamina = _stamina;
+        strength = _strength;
+        acuity = _acuity;
+
+        float speed = Mathf.Pow(agility, 2);
+        float health = stamina * 2.5f;
+        float damage = strength * 1.5f;
+        float criticalHitChance = acuity * 2.0f;
+    }
+
+    public void DisplayInfo()
+    {
+        Debug.Log(agility + stamina + strength + acuity);
+    }
+
+}
---
+++ 16T3-MDU112.2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerScript : MonoBehaviour
+{
+    void Start()
+    {
+        Character playerOne = new Character(UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11));
+        Character playerTwo = new Character(UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11));
+        playerOne.DisplayInfo();
+        playerTwo.DisplayInfo();
+    }
+}
