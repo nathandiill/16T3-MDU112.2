b35e3bdc9b0a694a177403ef927860e968cbdcfb
Nathan Diill <Nathan Diill>
2016-11-08 03:50:53 +1000

    2 teams of 3 are now successfully generated WITH comments.

--- 16T3-MDU112.2/Assets/Scripts/Character.cs
+++ 16T3-MDU112.2/Assets/Scripts/Character.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 
 public class Character {
 
+    // Assign random numbers to the attributes
     public int agility = UnityEngine.Random.Range(1, 11);
     public int stamina = UnityEngine.Random.Range(1, 11);
     public int strength = UnityEngine.Random.Range(1, 11);
     public int acuity = UnityEngine.Random.Range(1, 11);
 
+    // Create the stats
     public float speed;
     public float health;
     public float damage;
@@ -21,12 +23,14 @@ public class Character {
         strength = _strength;
         acuity = _acuity;
 
+        // Formula for the stats
         speed = Mathf.Pow(agility, 2);
         health = stamina * 2.5f;
         damage = strength * 1.5f;
         criticalHitChance = acuity * 2.0f;
     }
 
+    // Function to display a Character's stats
     public void DisplayInfo()
     {
         Debug.Log("Ag:" + agility + " (" + speed + "), " + "Sta:" + stamina + " (" + health + "), " + "Str:" + strength + " (" + damage + "), " + "Ac:" + acuity + " (" + criticalHitChance + ") ");
--- 16T3-MDU112.2/Assets/Scripts/PlayerScript.cs
+++ 16T3-MDU112.2/Assets/Scripts/PlayerScript.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 
 public class PlayerScript : MonoBehaviour
 {
-    void Start()
+    public void Start()
     {
+        // Create roster of 10 characters with stats
         Character playerOne = new Character(UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11));
         Character playerTwo = new Character(UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11));
         Character playerThree = new Character(UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11));
@@ -17,6 +18,56 @@ public class PlayerScript : MonoBehaviour
         Character playerNine = new Character(UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11));
         Character playerTen = new Character(UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11), UnityEngine.Random.Range(1, 11));
 
+        // List for Team One
+        List<Character> teamOneList = new List<Character>();
+        teamOneList.Add(playerOne);
+        teamOneList.Add(playerTwo);
+        teamOneList.Add(playerThree);
+        teamOneList.Add(playerFour);
+        teamOneList.Add(playerFive);
+
+        // List for Team Two
+        List<Character> teamTwoList = new List<Character>();
+        teamTwoList.Add(playerSix);
+        teamTwoList.Add(playerSeven);
+        teamTwoList.Add(playerEight);
+        teamTwoList.Add(playerNine);
+        teamTwoList.Add(playerTen);
+
+        // Selects UNIQUE Team One from the List of Characters
+        // Important! From this point on teamMember... is the Characters name
+        int chooseList1 = UnityEngine.Random.Range(0, 5);
+        Character teamOneMemberOne = teamOneList[chooseList1];
+        teamOneList.RemoveAt(chooseList1);
+        int chooseList2 = UnityEngine.Random.Range(0, 5);
+        Character teamOneMemberTwo = teamOneList[chooseList2];
+        teamOneList.RemoveAt(chooseList2);
+        int chooseList3 = UnityEngine.Random.Range(0, 5);
+        Character teamOneMemberThree = teamOneList[chooseList3];
+        teamOneList.RemoveAt(chooseList3);
+
+        // Selects UNIQUE Team Two from the List of Characters
+        int chooseList4 = UnityEngine.Random.Range(0, 5);
+        Character teamTwoMemberOne = teamTwoList[chooseList4];
+        teamTwoList.RemoveAt(chooseList4);
+        int chooseList5 = UnityEngine.Random.Range(0, 5);
+        Character teamTwoMemberTwo = teamTwoList[chooseList5];
+        teamTwoList.RemoveAt(chooseList5);
+        int chooseList6 = UnityEngine.Random.Range(0, 5);
+        Character teamTwoMemberThree = teamTwoList[chooseList6];
+        teamTwoList.RemoveAt(chooseList6);
+        
+        // Display initial values for each member pre-battle (health)
+        teamOneMemberOne.DisplayInfo();
+        teamOneMemberTwo.DisplayInfo();
+        teamOneMemberThree.DisplayInfo();
+        teamTwoMemberOne.DisplayInfo();
+        teamTwoMemberTwo.DisplayInfo();
+        teamTwoMemberThree.DisplayInfo();
+
+
+
+        // Call the display function for each Character
         playerOne.DisplayInfo();
         playerTwo.DisplayInfo();
         playerThree.DisplayInfo();
